f00fc3b4398856debaa8247129262251c0eb52f6
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-16 22:41:23 +0800

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,23 +1,20 @@
 const API_URL =
   "https://script.google.com/macros/s/AKfycbymfwC0xvRYxr_9T-25ih_fg7juZ_KjTWY4FElT6mTkT7WGMD5RQxtSBBYvyS4iDsIRcA/exec";
 
-// Search participant
 export async function getParticipant(id) {
   const response = await fetch(
     `${API_URL}?id=${encodeURIComponent(id)}`
   );
+
   return await response.json();
 }
 
-// Check-in participant
 export async function checkIn(id) {
-  const response = await fetch(API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ id }),
-  });
+
+  const response = await fetch(
+    `${API_URL}?action=checkin&id=${encodeURIComponent(id)}`
+  );
 
   return await response.json();
+
 }

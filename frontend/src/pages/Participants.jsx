607d75fc171c215185cd44df1f15c463f6ca3c02
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-16 22:03:29 +0800

    Update Participants.jsx

--- frontend/src/pages/Participants.jsx
+++ frontend/src/pages/Participants.jsx
@@ -1,23 +1,33 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 
-const participants = [
-  {
-    id: "ST001",
-    name: "Ahmad Ali",
-    organization: "USM",
-    status: "Registered",
-  },
-  {
-    id: "ST002",
-    name: "Siti Aminah",
-    organization: "UPM",
-    status: "Pending",
-  },
-];
+const API_URL =
+  "https://script.google.com/macros/s/AKfycbxdSRGkS1iyWCFZZUvSpmg6z8L2IsxwYEl9AQof4qxtF8mmJU2KEmVXSWJlvh1vS_7Fcg/exec";
 
 export default function Participants() {
+
+  const [participants, setParticipants] = useState([]);
+
+  useEffect(() => {
+
+    loadParticipants();
+
+  }, []);
+
+  async function loadParticipants() {
+
+    const response = await fetch(API_URL + "?action=list");
+    const data = await response.json();
+
+    setParticipants(data);
+
+  }
+
   return (
+
     <MainLayout>
+
       <h1>Participants</h1>
 
       <table
@@ -28,28 +38,52 @@ export default function Participants() {
           background: "white",
         }}
       >
+
         <thead>
+
           <tr style={{ background: "#4B0082", color: "white" }}>
             <th style={cell}>ID</th>
             <th style={cell}>Name</th>
             <th style={cell}>Organization</th>
             <th style={cell}>Status</th>
+            <th style={cell}>Action</th>
           </tr>
+
         </thead>
 
         <tbody>
+
           {participants.map((p) => (
+
             <tr key={p.id}>
+
               <td style={cell}>{p.id}</td>
               <td style={cell}>{p.name}</td>
               <td style={cell}>{p.organization}</td>
               <td style={cell}>{p.status}</td>
+
+              <td style={cell}>
+
+                <Link to={`/badge/${p.id}`}>
+                  <button>
+                    Print QR
+                  </button>
+                </Link>
+
+              </td>
+
             </tr>
+
           ))}
+
         </tbody>
+
       </table>
+
     </MainLayout>
+
   );
+
 }
 
 const cell = {

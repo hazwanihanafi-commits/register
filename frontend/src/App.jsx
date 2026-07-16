d4a4fcb8ea7c526763bbea69926c7975bccba32c
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-16 21:59:19 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-
+import Badge from "./pages/Badge";
 import Dashboard from "./pages/Dashboard";
 import Participants from "./pages/Participants";
 import Scanner from "./pages/Scanner";
@@ -12,6 +12,7 @@ export default function App() {
       <Route path="/participants" element={<Participants />} />
       <Route path="/scanner" element={<Scanner />} />
       <Route path="/reports" element={<Reports />} />
+      <Route path="/badge/:id" element={<Badge />} />
     </Routes>
   );
 }

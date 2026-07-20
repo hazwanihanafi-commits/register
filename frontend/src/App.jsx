a1440baef23b53b66ed228cf401531c610d6b9c6
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-20 09:23:58 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-
-import MainLayout from "./layouts/MainLayout";
-
 import Badge from "./pages/Badge";
 import Dashboard from "./pages/Dashboard";
 import Participants from "./pages/Participants";
@@ -11,14 +8,10 @@ import Reports from "./pages/Reports";
 export default function App() {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/participants" element={<Participants />} />
-        <Route path="/scanner" element={<Scanner />} />
-        <Route path="/reports" element={<Reports />} />
-      </Route>
-
-      {/* Badge page without sidebar */}
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/participants" element={<Participants />} />
+      <Route path="/scanner" element={<Scanner />} />
+      <Route path="/reports" element={<Reports />} />
       <Route path="/badge/:id" element={<Badge />} />
     </Routes>
   );

32f6fd888e8dc9602cc088c4033884a17ad32007
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-21 11:43:22 +0800

    Update certificateApi.js

--- frontend/src/services/certificateApi.js
+++ frontend/src/services/certificateApi.js
@@ -1,40 +1,46 @@
 const API_URL =
   "https://script.google.com/macros/s/AKfycbwL0N7FbMP7yoUKQ6FgrTPzIHrKesDkoD_EdIBL80xoaR0SH5Uos2CrUOg5kFtTAXiPUw/exec";
 
+// Generate Certificate
 export async function generateCertificate(id) {
 
   const response = await fetch(API_URL, {
+
     method: "POST",
 
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
 
     body: JSON.stringify({
       action: "generateCertificate",
-      id: id
-    })
+      id: id,
+    }),
 
   });
 
   return await response.json();
+
 }
 
+// Send Certificate Email
 export async function sendCertificate(id) {
 
   const response = await fetch(API_URL, {
+
     method: "POST",
 
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
 
     body: JSON.stringify({
       action: "sendCertificateEmail",
-      id: id
-    })
+      id: id,
+    }),
 
   });
 
   return await response.json();
+
 }

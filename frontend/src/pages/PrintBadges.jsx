aac4885afb6cbe1791b155a6c41aa35cb69d99b8
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-08-07 15:51:34 +0800

    Create PrintBadges.jsx

---
+++ frontend/src/pages/PrintBadges.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import BadgeCard from "../components/BadgeCard";
+import "./PrintBadges.css";
+
+const API_URL =
+  "https://script.google.com/macros/s/AKfycbwL0N7FbMP7yoUKQ6FgrTPzIHrKesDkoD_EdIBL80xoaR0SH5Uos2CrUOg5kFtTAXiPUw/exec";
+
+export default function PrintBadges() {
+
+  const [participants, setParticipants] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadParticipants();
+  }, []);
+
+  async function loadParticipants() {
+
+    try {
+
+      const response = await fetch(
+        `${API_URL}?action=list`
+      );
+
+      const data = await response.json();
+
+      setParticipants(data);
+
+    } catch (err) {
+
+      console.error(err);
+
+    } finally {
+
+      setLoading(false);
+
+    }
+
+  }
+
+  useEffect(() => {
+
+    if (!loading && participants.length > 0) {
+
+      setTimeout(() => {
+
+        window.print();
+
+      }, 1500);
+
+    }
+
+  }, [loading, participants]);
+
+  if (loading) {
+
+    return (
+
+      <div
+        style={{
+          padding:50,
+          textAlign:"center",
+          fontSize:24,
+          fontWeight:"bold"
+        }}
+      >
+        Preparing badges...
+      </div>
+
+    );
+
+  }
+
+  return (
+
+    <div className="print-container">
+
+      {participants.map((participant) => (
+
+        <div
+          className="print-page"
+          key={participant.id}
+        >
+
+          <BadgeCard participant={participant} />
+
+        </div>
+
+      ))}
+
+    </div>
+
+  );
+
+}
